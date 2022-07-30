058dfb49d1437a7f57e9c2da87534a174bf88e3a
leo1982 <leoscalesi@hotmail.com>
2022-07-30 12:13:39 -0300

    Se agrega Formulario RECEPCIONISTA_PRINCIPAL

---
+++ Clinica Medica/Clinica Medica/RECEPCIONISTA_PRINCIPAL.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RECEPCIONISTA_PRINCIPAL.aspx.cs" Inherits="Clinica_Medica.RECEPCIONISTA_PRINCIPAL" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Gestión de Turnos</h2>
+        </div>
+        <div>
+            <asp:Label ID="lblHolaRecepcionista" runat="server" Text="Bienvenido/a "></asp:Label>
+        </div>
+        <div>
+            <asp:Button ID="btnCargarAgenda" runat="server" Text="Cargar Agenda" />
+        </div>
+        <div>
+            <asp:Button ID="btnBrindarTurnos" runat="server" Text="Brindar Turnos" />
+        </div>
+        <div>
+            <asp:Button ID="btnPacientes" runat="server" Text="Lista de Pacientes" />
+        </div>
+        <div>
+            <asp:Button ID="btnAltaPacientes" runat="server" Text="Alta de Pacientes" />
+        </div>
+        <div>
+            <asp:Button ID="btnMedicos" runat="server" Text="Lista de Médicos" />
+        </div>
+        <div>
+            <asp:Button ID="btnAltaMedicos" runat="server" Text="Alta de Médicos" />
+        </div>
+        <div>
+            <asp:Button ID="btnSalir" runat="server" Text="Salir" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Clinica Medica/Clinica Medica/RECEPCIONISTA_PRINCIPAL.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Clinica_Medica
+{
+
+
+    public partial class RECEPCIONISTA_PRINCIPAL
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblHolaRecepcionista.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblHolaRecepcionista;
+
+        /// <summary>
+        /// Control btnCargarAgenda.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCargarAgenda;
+
+        /// <summary>
+        /// Control btnBrindarTurnos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBrindarTurnos;
+
+        /// <summary>
+        /// Control btnPacientes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPacientes;
+
+        /// <summary>
+        /// Control btnAltaPacientes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAltaPacientes;
+
+        /// <summary>
+        /// Control btnMedicos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMedicos;
+
+        /// <summary>
+        /// Control btnAltaMedicos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAltaMedicos;
+
+        /// <summary>
+        /// Control btnSalir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSalir;
+    }
+}
