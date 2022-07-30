67534ca5ddccd03d6ee1aa6989cd5d61becd2e0c
leo1982 <leoscalesi@hotmail.com>
2022-07-30 15:07:22 -0300

    Se agrega Proyecto Dominio (Biblioteca de Clases) con Clase Usuario y Clase Rol

---
+++ Clinica Medica/Dominio/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public Rol Rol { get; set; }
+
+
+    }
+}
