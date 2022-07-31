fb36b43eea72db9d26d008b7e603480ae8f16e81
leo1982 <leoscalesi@hotmail.com>
2022-07-31 16:22:09 -0300

    Se agrega Proyecto AccesoDatos con sus respectivos metodos

---
+++ Clinica Medica/AccesoDatos/AccesoDatos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AccesoDatos
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        //EN EL CONSTRUCTOR ARMO EL ConnectionString
+        //INSTANCIO UN OBJETO DE TIPO SqlCommand
+        public AccesoDatos()        
+        {
+            conexion = new SqlConnection("server=(local)\\SQLEXPRESS ; database=Clinica_Medica; integrated security= true");
+            comando = new SqlCommand();
+        }
+
+        //HAGO EL GET DEL LECTOR
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        //LE PASO AL COMANDO LA CONSULTA
+        public void setearConsulta (string consulta)
+        {
+            //LE DIGO QUE MI CONSULTA VIENE DESDE UN STRING
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+
+        public void ejecutarLectura()
+        {
+            //LE PASO AL COMNADO EL Connection String
+            comando.Connection = conexion;
+
+            //EJECUTO Y GURADO EN UN OBJETO DE TIPO SqlDataReader
+            //EL RESULTADO DE LA CONSULTA
+
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    
+    }
+}
